96d01f91efb06aa85215d0dfa71ed82e9d9dbbf8
pmuncey <munce93@gmail.com>
2017-01-31 22:49:13 -0600

    final

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -8,18 +8,22 @@ class GeocodingController < ApplicationController
 
   def street_to_coords
     @street_address = params[:user_street_address]
+    @street_address_with_pluses = @street_address.gsub(" ","+")
 
     # ==========================================================================
     # Your code goes below.
     #
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + @street_address_with_pluses # root url + whatever is after question mark
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
 
 
 
-    @latitude = "Replace this string with your answer."
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
 
-    @longitude = "Replace this string with your answer."
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
     render("geocoding/street_to_coords.html.erb")
   end
--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -8,24 +8,35 @@ class MeteorologistController < ApplicationController
 
   def street_to_weather
     @street_address = params[:user_street_address]
-
+    @street_address_with_pluses = @street_address.gsub(" ","+")
     # ==========================================================================
     # Your code goes below.
     #
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + @street_address_with_pluses # root url + whatever is after question mark
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
 
 
 
-    @current_temperature = "Replace this string with your answer."
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"].to_s
 
-    @current_summary = "Replace this string with your answer."
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"].to_s
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    url2 = "https://api.darksky.net/forecast/68187f0070d933bb7326e26e5701bd6a/" + @latitude +"," + @longitude
+    raw_data2 = open(url2).read
+    parsed_data2 = JSON.parse(raw_data2)
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @current_temperature = parsed_data2["currently"]["temperature"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @current_summary = parsed_data2["currently"]["summary"]
+
+    @summary_of_next_sixty_minutes = parsed_data2["minutely"]["summary"]
+
+    @summary_of_next_several_hours = parsed_data2["hourly"]["summary"]
+
+    @summary_of_next_several_days = parsed_data2["daily"]["summary"]
 
     render("meteorologist/street_to_weather.html.erb")
   end
